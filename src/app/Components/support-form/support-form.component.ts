b12239d9c420575c00824a88964e345a0ad1f6e6
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2022-10-14 17:30:35 -0500

    save to move to a new work station

--- src/app/Components/support-form/support-form.component.ts
+++ src/app/Components/support-form/support-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Ticket } from 'src/app/ticket.model';
 import { TicketService } from 'src/app/ticket.service';
-
 @Component({
   selector: 'app-support-form',
   templateUrl: './support-form.component.html',
@@ -9,13 +10,22 @@ import { TicketService } from 'src/app/ticket.service';
 })
 export class SupportFormComponent implements OnInit {
 
-  constructor(private ticketService:TicketService) { }
+  constructor(private ticketService: TicketService, private router:Router, ) { }
 
   ngOnInit(): void {
   }
-  onSubmitForm(form:NgForm){
-this.ticketService.ticket = (form.value.fullName, form.value.companyName, form.value.email, form.value.phoneNumber,form.value.brand, form.value.description)
-console.log(this.ticketService.ticket)
-}
+  onSubmitForm(form: NgForm) {
+    this.ticketService.ticket = ({
+      fullname: form.value.fullName,
+      companyName: form.value.companyName,
+      email: form.value.email,
+      phoneNumber: form.value.phoneNumber,
+      makeModel: form.value.makeModel,
+      description: form.value.description
+    })
+
+    console.log(this.ticketService.ticket)
+    this.router.navigateByUrl("/typeOfMachine")
+  }
 
 }
